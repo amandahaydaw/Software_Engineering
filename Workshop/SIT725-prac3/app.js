2bc774c483829bc0e5725d8783e969065d188462
amanda.haydaw <amanda.haydaw@accenture.com>
2020-07-29 18:24:38 +1000

    add folder

---
+++ Workshop/SIT725-prac3/app.js
@@ -0,0 +1,29 @@
+let express=require('express')
+let moment=require('moment')
+let app=express()
+
+
+app.use(express.static(__dirname+'/public'));
+
+var log=function(message){
+    var time=moment().format()
+    console.log('[Server] @'+time +''+ message)
+}
+let addition=function(num1,num2)
+{
+    let result=num1+num2;
+    return result;
+}
+
+app.get('/adder',function(req,res)
+
+{
+    let num1=parseInt(req.query.num1);
+    let num2=parseInt(req.query.num2);
+    let myresult = num1+num2;
+    res.json({result:myresult})
+})
+
+var port=3000;
+app.listen(port)
+log('server listening on'+port)
